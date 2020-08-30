285f14227bc1fb88338e54abdeac5ce61d880d38
Eduardo Ramos <eduardorxs@gmail.com>
2020-08-30 20:54:57 -0300

    EventEmitter, Input, Output

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css']
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  enteredValue = ''
-  newPost = 'SEM CONTEÚDO';
+  enteredTitle = '';
+  enteredContent = '';
+
+  // para criar um evento de emissao
+  @Output() postCreated = new EventEmitter();
 
   onAddPost() {
-    this.newPost = this.enteredValue;
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent,
+    };
+
+    // Emite um evento para um componenente que esteja em listening
+    this.postCreated.emit(post);
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   templateUrl: './post-list.component.html',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css'],
 })
 export class PostListComponent {
-  posts = [];
+  // Permite receber valor de props
+  @Input() posts = [];
 }
