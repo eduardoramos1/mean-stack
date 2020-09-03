28ab65b44dd1bac1e7dd16b1c5a29e177b25fc70
Eduardo Ramos <eduardorxs@gmail.com>
2020-09-02 23:38:40 -0300

    Template driven form

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Post } from './../post.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -13,10 +14,11 @@ export class PostCreateComponent {
   // para criar um evento de emissao
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (!form.valid) return;
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
 
     // Emite um evento para um componenente que esteja em listening
