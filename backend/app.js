cc2e0865229d06f5686461a9f4b516f5168b5c0b
Eduardo Ramos <eduardorxs@gmail.com>
2020-09-06 22:08:11 -0300

    Melhorado server.js e add nodemon

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 
 app.use((req, res, next) => {
-  console.log("Primeiro Middleware");
+  console.log("Primeiro Middleware teste");
   next();
 });
 
--- server.js
+++ server.js
@@ -1,9 +1,48 @@
 const http = require("http");
+const debug = require("debug")("node-angular");
 const app = require("./backend/app");
 
-const port = process.env.PORT || 3000;
+const normalizePort = (val) => {
+  var port = parseInt(val, 10);
 
+  if (isNaN(port)) return val;
+
+  if (port >= 0) return port;
+
+  return false;
+};
+
+const onError = (error) => {
+  if (error.syscall !== "listen") throw error;
+
+  const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " Requer Privilégios mais altos");
+      process.exit(1);
+      break;
+
+    case "EADDRINUSE":
+      console.error(bind + " Já está em uso");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe" + addr : "port" + port;
+  debug("Escutando em " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
 app.set("port", port);
+
 const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
 
 server.listen(port);
