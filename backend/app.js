d1a4b2778ae7173940c8255e3d9c332c35a3ce26
Eduardo Ramos <eduardorxs@gmail.com>
2020-09-12 19:55:17 -0300

    Tratamento de cors

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,19 @@
 const app = require("express")();
 
+app.use((req, res, next) => {
+  // corrige erro de Cors
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
 app.use("/api/posts", (req, res, next) => {
   const posts = [
     {
