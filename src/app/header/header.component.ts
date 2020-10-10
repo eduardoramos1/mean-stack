7341b1fd0bca49229f03b0d557e116b225a62b0f
Luis Eduardo Ramos <eduardorxs@gmail.com>
2020-10-10 19:13:52 -0300

    Add estilo para header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   templateUrl: './header.component.html',
   selector: 'app-header',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {}
