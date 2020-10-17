499cf91966aae91255cb8bf8bc25dfeb99a1c21b
Luis Eduardo Ramos <eduardorxs@gmail.com>
2020-10-17 18:26:11 -0300

    Criando rotas no backend

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const { error } = require("console");
 const mongoose = require("mongoose");
 
-const Post = require("./models/post");
+const postRoutes = require("./routes/posts");
 
 mongoose
   .connect(
@@ -36,62 +36,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-
-  post.save().then((result) => {
-    res.status(201).json({
-      message: "Post incluido com sucesso",
-      post: result,
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post).then((res) => {
-    console.log(res);
-    res.status(200).json({ message: "Post atualizado!" });
-  });
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find()
-    .then((posts) => {
-      console.log(posts);
-      res.status(200).json({
-        message: "Posts retornados com sucesso!",
-        posts,
-      });
-    })
-    .catch((err) => {
-      console.log("Não foi possivel buscar dados");
-      console.error(err);
-    });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post não encontrado" });
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post excluído com sucesso" });
-  });
-});
+app.use("/api/posts", postRoutes);
 
 module.exports = app;
---
+++ backend/routes/posts.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const Post = require("./../models/post");
+
+const router = express.Router();
+
+router.post("", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+
+  post.save().then((result) => {
+    res.status(201).json({
+      message: "Post incluido com sucesso",
+      post: result,
+    });
+  });
+});
+
+router.put("/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+  });
+  Post.updateOne({ _id: req.params.id }, post).then((res) => {
+    console.log(res);
+    res.status(200).json({ message: "Post atualizado!" });
+  });
+});
+
+router.get("", (req, res, next) => {
+  Post.find()
+    .then((posts) => {
+      console.log(posts);
+      res.status(200).json({
+        message: "Posts retornados com sucesso!",
+        posts,
+      });
+    })
+    .catch((err) => {
+      console.log("Não foi possivel buscar dados");
+      console.error(err);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  Post.findById(req.params.id).then((post) => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post não encontrado" });
+    }
+  });
+});
+
+router.delete("/:id", (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id }).then((result) => {
+    console.log(result);
+    res.status(200).json({ message: "Post excluído com sucesso" });
+  });
+});
+
+module.exports = router;
