b43ef17057e4c951d427580abf3949a2311c51b6
Luis Eduardo Ramos <eduardorxs@gmail.com>
2020-11-20 01:32:09 -0300

    Preparação para implementar paginação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
@@ -37,6 +38,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatExpansionModule,
     MatProgressSpinnerModule,
     HttpClientModule,
+    MatPaginatorModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -3,6 +3,7 @@ import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { Post } from './../post.model';
 
 import { Subscription } from 'rxjs';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   templateUrl: './post-list.component.html',
@@ -13,6 +14,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   // Permite receber valor de props
   posts: Post[] = [];
   loading: boolean = false;
+  totalPosts: number = 10;
+  postsPerPage: number = 2;
+  pageSizeOptions: number[] = [1, 2, 5, 10, 25];
 
   // Permite que o observable desapareça quando o componente deixar de existir, se não o observarble sempre estará ativo. Criando uma memory leak
   private postsSub: Subscription;
@@ -33,6 +37,10 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   onDelete(id: string) {
     this.postService.deletePost(id);
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
   }
 
   ngOnDestroy() {
